853eab88222740f2704ddefd5e68a528b0e17279
William Rodriguez <william@vivogaming.com>
2018-04-20 20:08:41 -0600

    Added post routes, default HTML elements

--- Main.js
+++ Main.js
@@ -2,6 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
+app.post('/encrypt', function(req, res) {
+
+    console.log(req);
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/decrypt', function(req, res) {
+
+    console.log(req);
+
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
