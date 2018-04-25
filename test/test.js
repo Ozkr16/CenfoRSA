5e9c160e63627c67193fcbf8aeb28a9b1b756258
Oscar Gutierrez C <>
2018-04-24 18:37:39 -0600

    Adding unit tests.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var RSA = require('encriptacion');
 
-describe('Encritacion', function() {
+describe('Encriptacion', function() {
   describe('GCD', function() {
     it('Debe retornar 1 para el máximo común divisor entre 5 y 3. ', function() {
       assert.equal(RSA.GCD(5,3), 1);
@@ -9,10 +9,58 @@ describe('Encritacion', function() {
   });
 });
 
-describe('Encritacion', function() {
-    describe('GCD', function() {
-      it('Debe retornar 1 para el máximo común divisor entre 571 y 491. ', function() {
-        assert.equal(RSA.GCD(571,491), 1);
-      });
+describe('Encriptacion', function() {
+  describe('GCD', function() {
+    it('Debe retornar 1 para el máximo común divisor entre 571 y 491. ', function() {
+      assert.equal(RSA.GCD(571,491), 1);
     });
   });
+});
+
+describe('Encriptacion', function() {
+  describe('RSAEncrypt', function() {
+    it('Debe retornar "w4/Cm8WERw==" al encriptar la palabra "hola". ', function() {
+      assert.equal(RSA.RSAEncrypt("hola"), "w4/Cm8WERw==");
+    });
+  });
+});
+
+describe('Encriptacion', function() {
+  describe('RSAEncrypt', function() {
+    it('Debe retornar "xJTClcKPw5XCm3vClcS0" al encriptar la palabra "Cenfotec". ', function() {
+      assert.equal(RSA.RSAEncrypt("Cenfotec"), "xJTClcKPw5XCm3vClcS0");
+    });
+  });
+});
+
+describe('Encriptacion', function() {
+  describe('RSADecrypt', function() {
+    it('Debe retornar "hola" al encriptar la palabra "w4/Cm8WERw==". ', function() {
+      assert.equal(RSA.RSADecrypt("w4/Cm8WERw=="), "hola");
+    });
+  });
+});
+
+describe('Encriptacion', function() {
+  describe('RSADecrypt', function() {
+    it('Debe retornar "Cenfotec" al encriptar la palabra "xJTClcKPw5XCm3vClcS0". ', function() {
+      assert.equal(RSA.RSADecrypt("xJTClcKPw5XCm3vClcS0"), "Cenfotec");
+    });
+  });
+});
+
+describe('Encriptacion', function() {
+  describe('RSAEncrypt vs RSADecrypt', function() {
+    it('Debe retornar "hola" al encriptar y desencriptar la palabra "hola". ', function() {
+      assert.equal(RSA.RSADecrypt(RSA.RSAEncrypt("hola")), "hola");
+    });
+  });
+});
+
+describe('Encriptacion', function() {
+  describe('RSAEncrypt vs RSADecrypt', function() {
+    it('Debe retornar "Cenfotec" al encriptar y desencriotar la palabra "Cenfotec". ', function() {
+      assert.equal(RSA.RSADecrypt(RSA.RSAEncrypt("Cenfotec")), "Cenfotec");
+    });
+  });
+});
